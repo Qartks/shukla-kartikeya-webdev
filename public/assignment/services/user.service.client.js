699854bd1a926d64ac2a5c997c996cef117b7c5b
Kartikeya Shukla <kshukla@ccs.neu.edu>
2016-10-10 01:50:50 -0400

    Made a progress on the user pages, including services and controllers

--- public/assignment/services/user.service.client.js
+++ public/assignment/services/user.service.client.js
@@ -4,46 +4,77 @@
         .factory("UserService", UserService);
 
     function UserService() {
+        var idCount = 555;
         var users = [
-            {_id: "123", username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder"},
-            {_id: "234", username: "bob", password: "bob", firstName: "Bob", lastName: "Marley"},
+            {_id: "123", username: "alice", password: "alice", firstName: "Alice", lastName: "Wonder", email: "alicewonder@Mhatter.com"},
+            {_id: "234", username: "bob", password: "bob", firstName: "Bob", lastName: "Marley", email: "bob@bob.com"},
             {_id: "345", username: "charly", password: "charly", firstName: "Charly", lastName: "Garcia"},
             {_id: "456", username: "jannunzi", password: "jannunzi", firstName: "Jose", lastName: "Annunzi"}
         ];
 
-        var api = {
-            "createUser": "createUser",
-            "findUserById": "findUserById",
-            "findUserByUsername": "findUserByUsername",
-            "findUserByCredentials": "findUserByCredentials",
-            "updateUser": "updateUser",
-            "deleteUser": "deleteUser"
+        return {
+            createUser: createUser,
+            findUserById: findUserById,
+            findUserByUsername: findUserByUsername,
+            findUserByCredentials: findUserByCredentials,
+            updateUser: updateUser,
+            deleteUser: deleteUser,
+            users : users
         };
-
-        return api;
         
         function createUser(user) {
+            user._id = idCount++;
+            users.push(user);
+            return user;
 
         }
         
         function findUserById(userId) {
-
+            for (var key in users) {
+                if (users.hasOwnProperty(key)) {
+                    if (users[key]._id == userId) {
+                        return users[key];
+                    }
+                }
+            }
         }
 
         function findUserByUsername(username) {
-
+            for (var key in users) {
+                if (users.hasOwnProperty(key)) {
+                    if (users[key].username === username) {
+                        return users[key];
+                    }
+                }
+            }
         }
 
         function findUserByCredentials(username, password) {
-
+            for (var key in users) {
+                if (users.hasOwnProperty(key)) {
+                    if ((users[key].username === username) && (users[key].password === password)) {
+                        return users[key];
+                    }
+                }
+            }
         }
 
         function updateUser(userId, user) {
-
+            for (var key in users) {
+                if (users.hasOwnProperty(key)) {
+                    if (users[key]._id === userId) {
+                        users[key] = user;
+                    }
+                }
+            }
+            console.log(users);
         }
 
         function deleteUser(userId) {
-
+            var index = users.indexOf(users._id === userId);
+            if (index > -1) {
+                users.splice(index, 1);
+            }
         }
 
 
--- public/assignment/views/user/user.controller.client.js
+++ public/assignment/views/user/user.controller.client.js
@@ -5,30 +5,46 @@
         .controller("RegisterController", RegisterController)
         .controller("ProfileController", ProfileController);
 
-    function LoginController($scope, UserService) {
+    function LoginController($location, UserService) {
         var vm = this;
+        vm.login = login;
 
         function login(user) {
+            console.log(user);
             user = UserService.findUserByCredentials(user.username, user.password);
+            console.log(user);
             if(user) {
                 $location.url("/user/" + user._id);
             } else {
-                vm.alert = "Unable to login";
+                alert("Unable to login");
             }
         }
     }
 
-    function RegisterController() {
+    function RegisterController($location, UserService) {
         var vm = this;
+        vm.register = function (user) {
+            user = UserService.createUser(user);
+            if(user) {
+                $location.url("/user/" + user._id);
+            } else {
+                alert("Unable to Register");
+            }
+        }
     }
 
     function ProfileController($routeParams, UserService) {
         var vm = this;
-        vm.userId = $routeParams["userId"];
+        vm.userId = $routeParams.uid;
         function init() {
             vm.user = UserService.findUserById(vm.userId);
         }
         init();
+        
+        vm.update = function (user) {
+            UserService.updateUser(vm.userId, user);
+        }
+
     }
 
 })();
