db0f69eba8af71667f3f4fcc27b701e275daabb4
Kartikeya Shukla <shukla.ka@husky.neu.edu>
2016-11-11 20:59:03 -0500

    Changed the home page again

--- public/Js/jquery.js
+++ public/Js/jquery.js
@@ -1,5 +1,7 @@
 
 
+ // When Loading
+
 $("#my-header")
     .velocity("fadeIn", { duration: 1500 });
 
@@ -7,33 +9,10 @@ $("#bottom-section")
     .velocity("slideDown", { duration: 1500 })
 
 
-$(window).scroll(function() {
-    if ($(".navbar").offset().top > 50) {
-        $(".navbar-fixed-top").addClass("top-nav-collapse");
-    } else {
-        $(".navbar-fixed-top").removeClass("top-nav-collapse");
-    }
-});
-
-//jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {
-    $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
-});
-
-
+ // When Document is loaded
 $(document).ready(function(){
 
-    function withVelocity() {
-    }
 
-    $("#switch-animation").click(function(){
-        withVelocity();
-    });
+
 });
 
